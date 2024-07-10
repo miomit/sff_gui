434966e19a1f48acd5ec25837a25ef6d7371e291
Yura Movsesyan <movsesyan-03@inbox.ru>
2024-07-10 21:50:37 +0300

    init window

---
+++ lib/window.dart
@@ -0,0 +1,39 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:sff_gui/pages/yaru_page.dart';
+import 'package:sff_gui/theme.dart';
+import 'package:yaru/yaru.dart';
+
+class Window extends StatelessWidget {
+  const Window({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return YaruTheme(
+      data: YaruThemeData(
+        variant: InheritedYaruVariant.of(context),
+      ),
+      builder: (context, yaru, child) {
+        return MaterialApp(
+          title: 'Yaru',
+          debugShowCheckedModeBanner: false,
+          theme: yaru.theme,
+          darkTheme: yaru.darkTheme,
+          highContrastTheme: yaruHighContrastLight,
+          highContrastDarkTheme: yaruHighContrastDark,
+          home: const YaruPage(),
+          scrollBehavior: const MaterialScrollBehavior().copyWith(
+            dragDevices: {
+              PointerDeviceKind.mouse,
+              PointerDeviceKind.touch,
+              PointerDeviceKind.stylus,
+              PointerDeviceKind.unknown,
+              PointerDeviceKind.trackpad,
+            },
+          ),
+        );
+      },
+    );
+  }
+}
